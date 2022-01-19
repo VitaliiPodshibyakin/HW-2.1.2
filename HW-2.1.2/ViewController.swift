e58407f55bf478a62981e5c5de6d1206100426b3
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-01-19 20:10:38 +0300

    Setup logic

--- HW-2.1.2/ViewController.swift
+++ HW-2.1.2/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    var light = "red"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 60
+        redLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = 60
+        yellowLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = 60
+        greenLightView.alpha = 0.3
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPress() {
+        switch light {
+        case "red":
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            light = "yellow"
+            startButton.setTitle("NEXT", for: .normal)
+        case "yellow":
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+            light = "green"
+        case "green":
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            light = "red"
+        default:
+            break
+        }
+    }
+    
 }
 
