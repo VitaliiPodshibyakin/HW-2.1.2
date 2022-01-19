b72681d4f3b8ce1bdd6ac45899b2a9031070c57c
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-01-19 19:34:49 +0300

    Initial Commit

---
+++ HW-2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-2.1.2
+//
+//  Created by Виталий Подшибякин on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
